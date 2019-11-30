196e8e3bcf8f8df202fcc12751bce27b867fa21e
AndersonHTN <56129719+AndersonHTN@users.noreply.github.com>
2019-11-30 16:41:47 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0x2b297cc8F221FB579e8b290A4f6fFEb019944DBe";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
